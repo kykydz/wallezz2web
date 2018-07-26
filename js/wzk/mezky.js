14242f95aa138fdbe6c6833b073c5e9953546d64
zky <zkyhersa@gmail.com>
2018-07-26 15:52:25 +0700

    added accordion and scroll

---
+++ js/wzk/mezky.js
@@ -0,0 +1,43 @@
+var acc     = document.getElementsByClassName('accord-title');
+      var panel   = document.getElementsByClassName('accord-panel');
+      var i, j = 0;
+      var accIndex = 0;
+      for (i = 0; i < acc.length; i++){
+          acc[0].addEventListener("click", function(){
+            panel[1].style.maxHeight = "0px";
+            panel[2].style.maxHeight = "0px";
+            panel[0].style.maxHeight = "200px";
+          });
+          acc[1].addEventListener("click", function(){
+            panel[0].style.maxHeight = "0px";
+            panel[2].style.maxHeight = "0px";
+            panel[1].style.maxHeight = "200px";
+          });
+          acc[2].addEventListener("click", function(){
+            panel[0].style.maxHeight = "0px";
+            panel[1].style.maxHeight = "0px";
+            panel[2].style.maxHeight = "200px";
+          });
+      }
+      
+      // for (j = 0; j < panel.length; j++){
+      //   panel[j].style.maxHeight = "0px";
+      // }
+      // accIndex++;
+
+      // for (i = 0; i < acc.length; i++) {
+      //   acc[i].addEventListener("click", function(){
+      //     panel[accIndex].style.maxHeight = "0px";
+
+      //     var accPanel = this.nextElementSibling;
+      //     accPanel.style.maxHeight = "200px";
+
+      //     // if (accPanel.style.maxHeight) {
+      //     //   accPanel.style.maxHeight = null;
+      //     // } else {
+      //     //   accPanel.style.maxHeight = accPanel.scrollHeight + "px";
+      //     // }
+          
+      //   });
+      // }
+      // panel[accIndex-1].style.maxHeight = "200px";
